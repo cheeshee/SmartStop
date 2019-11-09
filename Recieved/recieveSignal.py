979b305ac035c6a0a7ec7156cbae51b2e14917b6
cheeshee <amyliu1.618@gmail.com>
2019-11-09 15:55:40 -0800

    added noFlash image

--- Recieved/recieveSignal.py
+++ Recieved/recieveSignal.py
@@ -1,16 +1,22 @@
-from PIL import Image
+import pygame
 from time import sleep
-        
+from pygame.locals import*
+
+pygame.init()        
 flash = Image.open('flash.gif')
 noFlash = Image.open('noFlash.jpg')
+WIDTH = 1280
+HEIGHT = 800
+windowSurface = pygame.display.set_mode((WIDTH, HEIGHT), 0, 0)
+
 
 # 0 flash       to stop
 # 1 no flash    to go
 def chooseIm(num):
     if (num == 0):
-        flash.show()
+        window.Surface.blit(flash, (0,0))
     elif (num == 1):
-        noFlash.show()
+        window.Surface.blit(noFlash, (0,0))
     sleep(0.5)
 
     
