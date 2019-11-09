32c2977bbfe9f7b4600174891abdfced57fc142a
cheeshee <amyliu1.618@gmail.com>
2019-11-09 14:39:21 -0800

    adding pi side recieve code

---
+++ Recieved/recieveSignal.py
@@ -0,0 +1,13 @@
+from PIL import Image
+        
+flash = Image.open('flash.gif')
+noFlash = Image.open('noFlash.jpg')
+
+# 0 flash       to stop
+# 1 no flash    to go
+def chooseIm(num):
+    if (num == 0):
+        flash.show()
+    elif (num == 1):
+        noFlash.show()
+
