cbd31580657721dbf56978c76a42a3a2f5ca9392
Amy Liu <amyliu1.618@gmail.com>
2019-11-09 16:09:05 -0800

    pygame update

--- Recieved/recieveSignal.py
+++ Recieved/recieveSignal.py
@@ -3,10 +3,10 @@ from time import sleep
 from pygame.locals import*
 
 pygame.init()        
-flash = Image.open('flash.gif')
-noFlash = Image.open('noFlash.jpg')
-WIDTH = 1280
-HEIGHT = 800
+flash = pygame.image.load('/home/pi/Congested/SmartStop/Recieved/flash.jpg')
+noFlash = pygame.image.load('/home/pi/Congested/SmartStop/Recieved/noFlash.jpg')
+WIDTH = 800
+HEIGHT = 480
 windowSurface = pygame.display.set_mode((WIDTH, HEIGHT), 0, 0)
 
 
@@ -14,11 +14,11 @@ windowSurface = pygame.display.set_mode((WIDTH, HEIGHT), 0, 0)
 # 1 no flash    to go
 def chooseIm(num):
     if (num == 0):
-        window.Surface.blit(flash, (0,0))
+        windowSurface.blit(flash, (0,0))
     elif (num == 1):
-        window.Surface.blit(noFlash, (0,0))
+        windowSurface.blit(noFlash, (0,0))
     sleep(0.5)
 
     
 
-chooseIm(1)
+chooseIm(0)
