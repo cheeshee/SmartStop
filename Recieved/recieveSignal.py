f43970d466b6d64a0b7abe22485f4d3cbe5ce7d3
cheeshee <amyliu1.618@gmail.com>
2019-11-09 16:48:24 -0800

    flashing added

--- Recieved/recieveSignal.py
+++ Recieved/recieveSignal.py
@@ -2,23 +2,30 @@ import pygame
 from time import sleep
 from pygame.locals import*
 
-pygame.init()        
-flash = pygame.image.load('/home/pi/Congested/SmartStop/Recieved/flash.png')
+pygame.init()
+flash0 = pygame.image.load('/home/pi/Congested/SmartStop/Recieved/flash0.png')
+flash1 = pygame.image.load('/home/pi/Congested/SmartStop/Recieved/flash1.png')
 noFlash = pygame.image.load('/home/pi/Congested/SmartStop/Recieved/noFlash.png')
 WIDTH = 800
 HEIGHT = 480
 windowSurface = pygame.display.set_mode((WIDTH, HEIGHT), 0, 0)
-
+flashing = 0
 
 # 0 flash       to stop
 # 1 no flash    to go
 def chooseIm(num):
-    if (num == 0):
-        windowSurface.blit(flash, (0,0))
+    global flashing
+    if (num == 0 and flashing == 0):
+        windowSurface.blit(flash0, (0,0))
+        flashing = 1
+    if (num == 0 and flashing == 1):
+        windowSurface.blit(flash1, (0,0))
+        flashing = 0
     elif (num == 1):
         windowSurface.blit(noFlash, (0,0))
-    sleep(0.5)
+        flashing = 0
     pygame.display.update()
+    sleep(0.5)
 
     
 
